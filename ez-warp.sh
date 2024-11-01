e99e26641fbed66d6e0e4c6cf19dd4ca04244176
jellyenderson <46281269+jellyenderson@users.noreply.github.com>
2024-11-02 03:27:34 +0330

    Update ez-warp.sh

--- ez-warp.sh
+++ ez-warp.sh
@@ -25,16 +25,15 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "This script requires root privileges. Please run it as root."
     exit 1
 fi
-#installing necessary packages
 
+#installing necessary packages
 apt update
 ubuntu_version=$(lsb_release -r | awk '{print $2}')
 if [[ "$ubuntu_version" == "24.04" ]]; then
-  sudo apt install -y wireguard
+  apt install -y wireguard
 elif [[ "$ubuntu_version" == "22.04" || "$ubuntu_version" == "20.04" ]]; then
-  sudo apt install -y wireguard-dkms wireguard-tools resolvconf
+  apt install -y wireguard-dkms wireguard-tools resolvconf
 fi
-
 
 #checking packages
 if ! command -v wg-quick &> /dev/null
@@ -51,18 +50,15 @@ fi
 clear
 #downloading assets
 arch=$(architecture)
-wget -O "/usr/bin/wgcf" https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_amd64$arch
+wget -O "/usr/bin/wgcf" "https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_${arch}"
 chmod +x /usr/bin/wgcf
-
-
 
 clear
 # removing files that might cause problems
-
 rm -rf wgcf-account.toml &> /dev/null || true
 rm -rf /etc/wireguard/warp.conf &> /dev/null || true
-# main dish
 
+# main dish
 wgcf register
 read -rp "Do you want to use your own key? (Y/n): " response
 if [[ $response =~ ^[Yy]$ ]]; then
@@ -72,30 +68,6 @@ if [[ $response =~ ^[Yy]$ ]]; then
 fi
 
 wgcf generate
-
-
-
-#creating config in the wireguard directory
-
-# this algorithm is  deprecated
-
-# PRIVATE_KEY=$(grep -oP 'PrivateKey\s*=\s*\K.*' wgcf-profile.conf)
-# cat << EOF > "/etc/wireguard/warp.conf"
-# [Interface]
-# PrivateKey = $PRIVATE_KEY
-# Address = 172.16.0.2/32
-# Address = 2606:4700:110:8a1a:85ef:da37:b891:8d01/128
-# DNS = 1.1.1.1
-# MTU = 1280
-# Table = off
-# [Peer]
-# PublicKey = bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
-# AllowedIPs = 0.0.0.0/0
-# AllowedIPs = ::/0
-# Endpoint = engage.cloudflareclient.com:2408
-# EOF
-
-# the better algorithm
 
 sed -i '/\[Peer\]/i Table = off' wgcf-profile.conf
 mv wgcf-profile.conf /etc/wireguard/warp.conf
